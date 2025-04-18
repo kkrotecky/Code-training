8830d52cd36cb4d4a59a19c771a87dbc098acc16
kkrotecky <kkrotecky@gmail.com>
2025-04-18 23:32:28 +0200

    This is the message that I will commit the changes.
    modified:   main.py

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 print("Hello World!")
 # This is a test to see if the code is running correctly.
+print("This is only one of my tests.")
+print("Let's see if everything works")
+print("What is your name?")
+name = input("Enter your name:")
+print ("Hello " + name + "!") 
+print("How do your like the wearther today?")
+weather = input("Is the weather nice? (Yes/No):")
+if weather.input() == "yes" or "y":
+    print("That's great to hear!")
+else:
+    print("I hope the weather gets better soon!")
+
+print("What is your favorite color?")
+color = input("Enter your favorite color:")
+print("Your favorite color is " + color + "!")
+
+print("I hope you are having a great day " + name + "!")      
