36799a0d898d179feada9488af18408a6521b087
kkrotecky <kkrotecky@gmail.com>
2025-04-20 23:48:28 +0200

    Changes to be committed: new file:   Program3-kosci_game.py
    A start of a simple game which allows player to roll a dice.

---
+++ Program3-kosci_game.py
@@ -0,0 +1,24 @@
+import random
+
+def rzut_kostka():
+    return random.randit(1, 6)
+
+def gra_kosci():
+    print("Witaj, jak masz na imię?")
+    imie = input("Podaj swoje imię: ")
+    print(f"Witaj, {imie}!")
+    print("Zasady gry są następujące: rzucasz 2 razy kostką. Za każde wyrzucone oczko po pierwszm rzucie dostajesz 1 punkt.\nZa każde wyrzucone oczko po drugim rzucie dostajesz mnożnik, przez ilość wyrzuconych oczek.")
+    print("Przykładowo jeśli w pierwszym rzucie wyrzucisz 3 oczka, a w drugim 2 oczka, to pomnożymy 3*2=6.")
+    print("Jeśli wyrzucisz 1 oczko w pierwszym rzucie, a 2 oczka w drugim rzucie, to dostaniesz 1*2=2 punkty i tak dalej.")
+    print("Wygrywasz, jeżli zdobędziesz więcej punktów niż komputer.")
+    print("Czy chcesz zagrać? (tak/nie)")
+    odpowiedz = input("Podaj odpowiedź: ").lower()
+    if odpowiedz == "tak":
+        print("Super! Zaczynamy grę!")
+    else:
+        print("Dziękuję za grę! Do zobaczenia następnym razem!")
+        return False
+    return True
+    
+
+gra_kosci()
