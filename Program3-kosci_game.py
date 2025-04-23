6ee87d8f62128a95d9c86d073b238df9a02b8710
kkrotecky <kkrotecky@gmail.com>
2025-04-23 23:22:07 +0200

    Changes to be committed: modified:   Program3-kosci_game.py
    Program has now a 2 players multiplayer.
    For the next this could be extended to more players if necesary.

--- Program3-kosci_game.py
+++ Program3-kosci_game.py
@@ -4,30 +4,59 @@ def rzut_kostka():
     return random.randint(1, 6)
 
 def intro():
-    print("Witam! Jak masz na imię?")
-    imie = input("Twoje imie: ")
-    print("Witaj, " + imie + "! ")
+    print("Witam! Jak się nazywacie?")
+    imie1 = input("Imię gracza nr 1: ")
+    print("Witaj, " + imie1 + "! ")
+
+    imie2 = input("Imię gracza nr 2: ")
+    print("Witaj, " + imie2 + "! ")
+
+    return imie1, imie2
     
 
 def instrukcja():
-    print("Zasady gry są następujące: rzucasz 2 razy kostką. Za każde wyrzucone oczko po pierwszm rzucie dostajesz 1 punkt.\nZa każde wyrzucone oczko po drugim rzucie dostajesz mnożnik, przez ilość wyrzuconych oczek.")
-    print("Przykładowo jeśli w pierwszym rzucie wyrzucisz 3 oczka, a w drugim 2 oczka, to pomnożymy 3*2=6.")
-    print("Jeśli wyrzucisz 1 oczko w pierwszym rzucie, a 2 oczka w drugim rzucie, to dostaniesz 1*2=2 punkty i tak dalej.")
-    print("Wygrywasz, jeżli zdobędziesz więcej punktów niż komputer.")
+    
+    print("Zasady gry są następujące: każdy gracz rzuca 2 razy kostką. Za każde wyrzucone oczko po pierwszm rzucie dostaje 1 punkt.\nZa każde wyrzucone oczko po drugim rzucie otrzymuje mnożnik, przez ilość wyrzuconych oczek.")
+    print("Przykładowo jeśli w pierwszym rzucie gracz wyrzuci 3 oczka, a w drugim 2 oczka, to pomnożymy 3*2=6.")
+    print("Wygrywa gracz, który zdobędzie więcej punktów.")
 
 def core():
     rzut1 = rzut_kostka()
     rzut2 = rzut_kostka()
     wynik = rzut1 * rzut2
+
+    rzut3 = rzut_kostka()
+    rzut4 = rzut_kostka()
+    wynik2 = rzut3 * rzut4
     
-    print("Kliknij Enter by zacząć...")
+    print(f"Rozpoczyna {imie1}")
+    print("Kliknij Enter by rzucić kostką...")
     input()
     print(f"Pierwszy rzut: {rzut1}")
-    print("Kliknij Enter by zacząć...")
+    print("Kliknij Enter oddać drugi rzut...")
     input()
     print(f"Drugi rzut: {rzut2}")
-    print(f"Zdobyłeś: {wynik} punktów!")
 
-intro()
+    print(f"Brawo! Twój wynik to: {wynik} punktów!")
+
+    print(f"Kolej {imie2} ")
+
+    print("Kliknij Enter by rzucić kostką...")
+    input()
+    print(f"Pierwszy rzut: {rzut3}")
+    print("Kliknij Enter oddać drugi rzut...")
+    input()
+    print(f"Drugi rzut: {rzut4}")
+
+    print(f"Brawo! Twój wynik to: {wynik2} punktów!")
+
+    if wynik > wynik2:
+        print(f"Wygrywa {imie1} z {wynik} punktami!")
+    elif wynik2 > wynik:
+        print(f"Wygrywa {imie2} z {wynik2} punktami!")
+    else:
+        print("Remis! Obaj gracze zdobyli tyle samo punktów.")
+
+imie1, imie2 = intro()
 instrukcja()
 core()
