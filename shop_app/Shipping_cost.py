a18d0783b9bba79b2278d0381c4698ce75a89cf1
kkrotecky <94808427+kkrotecky@users.noreply.github.com>
2025-05-20 19:59:03 +0200

    modified:   shop_app/Shipping_cost.py 	new file:   shop_app/__pycache__/items.cpython-313.pyc 	new file:   shop_app/items.py 	new file:   test.py

--- shop_app/Shipping_cost.py
+++ shop_app/Shipping_cost.py
@@ -1,11 +1,12 @@
 import math
+import items
 
+#customer_basket_cost = 34
+#customer_basket_weight = 44
 
+#if customer_basket_cost >= 100:
+#    print("Your shipping is: $" + 0)
+#else:
+#    print("Your shipping is: $" + str(customer_basket_weight * 1.2))
 
-customer_basket_cost = 34
-customer_basket_weight = 44
-
-if customer_basket_cost >= 100:
-    print("Your shipping is: $" + 0)
-else:
-    print("Your shipping is: $" + str(customer_basket_weight * 1.2))
+print(len(items_def))
---
+++ shop_app/items.py
@@ -0,0 +1,55 @@
+def items_list():
+
+    items_def = {
+        "banana" : {
+            "weight" : 1,
+            "cost" : 2
+        },
+        "cherry" : {
+            "weight" : 0.5,
+            "cost": 1
+        },
+        "table": {
+            "weight" : 15,
+            "cost" : 250
+        },
+        "cookies": {
+            "weight" : 0.150,
+            "cost" : 2
+        },
+        "computer": {
+            "weight" : 13,
+            "cost" : 3500
+        },
+        "television": {
+            "weight" : 10,
+            "cost" : 1000
+        },
+        "fragrance": {
+            "weight" : 0.200,
+            "cost" : 600
+        },
+        "phone": {
+            "weight" : 0.100,
+            "cost" : 1200
+        },
+        "wine": {
+            "weight" : 1,
+            "cost" : 20
+        },
+        "headphones": {
+            "weight" : 0.200,
+            "cost" : 200
+        },
+        "keyboard": {
+            "weight" : 0.200,
+            "cost" : 350
+        },
+        "lamp": {
+            "weight" : 0.500,
+            "cost" : 50
+        },
+    }
+    print(len(items_def))
+
+items_list()
